9bfc62ebf4608587bd6679e145053534eacfb6c9
vivi <vivid_xiang@163.com>
2017-05-01 00:39:51 +0800

    log4j2 与spring 兼容

--- log4j2/src/main/java/com/bow/log4j2/Log4j2Test.java
+++ log4j2/src/main/java/com/bow/log4j2/Log4j2Test.java
@@ -3,6 +3,8 @@ package com.bow.log4j2;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * @author vv
@@ -15,5 +17,14 @@ public class Log4j2Test {
     @Test
     public void test1() {
         LOGGER.info("test {}", "nice");
+    }
+
+    /**
+     * spring 使用的commons-logging 依赖log4j 1.x .
+     * 因此用log4j2的时候需要增加一个兼容包log4j-1.2-api
+     */
+    @Test
+    public void test2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
     }
 }
